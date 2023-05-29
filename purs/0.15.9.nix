7a8d9ab1364a78f628ca7fd6a630f06b89fe3179
Thomas Honeyman <hello@thomashoneyman.com>
2023-05-29 13:06:56 -0400

    Add workflow file

--- purs/0.15.9.nix
+++ purs/0.15.9.nix
@@ -14,15 +14,15 @@
     };
     "aarch64-linux" = {
       url = "https://github.com/purescript/purescript/releases/download/v${version}/linux-arm64.tar.gz";
-      hash = "sha256-A8v0N75PGMT4fP9v3gUnm4ZmZDz7D+BM0As1TaeNS2U=";
+      hash = "";
     };
     "x86_64-darwin" = {
       url = "https://github.com/purescript/purescript/releases/download/v${version}/macos.tar.gz";
-      hash = "sha256-A8v0N75PGMT4fP9v3gUnm4ZmZDz7D+BM0As1TaeNS2U=";
+      hash = "sha256-1xxg79rlf7li9f73wdbwif1dyy4hnzpypy6wx4zbnvap53habq9f";
     };
     "aarch64-darwin" = {
       url = "https://github.com/purescript/purescript/releases/download/v${version}/macos-arm64.tar.gz";
-      hash = "sha256-A8v0N75PGMT4fP9v3gUnm4ZmZDz7D+BM0As1TaeNS2U=";
+      hash = "";
     };
   };
 
