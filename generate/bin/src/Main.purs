fa83a69dd6cd38a66f2d371e3154a31bf92e9683
Thomas Honeyman <hello@thomashoneyman.com>
2023-06-16 19:33:30 -0400

    Also check spago updates

--- generate/bin/src/Main.purs
+++ generate/bin/src/Main.purs
@@ -124,8 +124,8 @@ main = Aff.launchAff_ do
             -- We switch the manifest dir from the user-provided input to the
             -- cloned repo before we do anything else.
             Reader.local (\env -> env { manifestDir = Path.concat [ env.tmpDir, "purescript-nix", "manifests" ] }) do
-              if Map.isEmpty pursUpdates then
-                Console.log "No new purs releases."
+              if Map.isEmpty pursUpdates && Map.isEmpty spagoUpdates then
+                Console.log "No new releases."
               else do
                 Console.log "New purs releases, writing to disk..."
                 void $ AppM.runGitM Git.gitCloneUpstream
