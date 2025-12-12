66d9d29caa54081eea09db8c07bca222516bd41e
robo-trh <135880706+robo-trh@users.noreply.github.com>
2025-12-12 18:06:58 -0500

    Update purs (0.15.16-7, 0.15.16-8) and purs-tidy (0.11.1) (#122)
    * Skip patching for statically linked purs versions >= 0.15.16-7

--- manifests/build-support/mkPursDerivation.nix
+++ manifests/build-support/mkPursDerivation.nix
@@ -5,44 +5,55 @@
   gmp,
   fetchurl,
   ncurses5,
-}: {
+}:
+{
   version,
   url,
   hash,
-}: let
+}:
+let
   dynamic-linker = stdenv.cc.bintools.dynamicLinker;
 
-  patchelf = libPath:
-    if stdenv.isDarwin
-    then ""
-    else ''
-      chmod u+w $PURS
-      patchelf --interpreter ${dynamic-linker} --set-rpath ${libPath} $PURS
-      chmod u-w $PURS
-    '';
+  # Versions >= 0.15.16-7 are statically linked and don't need patching
+  needsPatching = lib.versionOlder version "0.15.16-7";
+
+  patchelf =
+    libPath:
+    if stdenv.isDarwin || !needsPatching then
+      ""
+    else
+      ''
+        chmod u+w $PURS
+        patchelf --interpreter ${dynamic-linker} --set-rpath ${libPath} $PURS
+        chmod u-w $PURS
+      '';
 in
-  stdenv.mkDerivation rec {
-    pname = "purs";
-    inherit version;
+stdenv.mkDerivation rec {
+  pname = "purs";
+  inherit version;
 
-    src = fetchurl {inherit url hash;};
+  src = fetchurl { inherit url hash; };
 
-    buildInputs = [zlib gmp ncurses5];
-    libPath = lib.makeLibraryPath buildInputs;
-    dontStrip = true;
+  buildInputs = lib.optionals needsPatching [
+    zlib
+    gmp
+    ncurses5
+  ];
+  libPath = lib.makeLibraryPath buildInputs;
+  dontStrip = true;
 
-    installPhase = ''
-      mkdir -p $out/bin
-      PURS="$out/bin/purs"
-      install -D -m555 -T purs $PURS
-      ${patchelf libPath}
-      mkdir -p $out/etc/bash_completion.d/
-      $PURS --bash-completion-script $PURS > $out/etc/bash_completion.d/purs-completion.bash
-    '';
+  installPhase = ''
+    mkdir -p $out/bin
+    PURS="$out/bin/purs"
+    install -D -m555 -T purs $PURS
+    ${patchelf libPath}
+    mkdir -p $out/etc/bash_completion.d/
+    $PURS --bash-completion-script $PURS > $out/etc/bash_completion.d/purs-completion.bash
+  '';
 
-    meta = with lib; {
-      description = "Compiler for a strongly-typed language that compiles to JavaScript";
-      homepage = "https://github.com/purescript/purescript";
-      mainProgram = pname;
-    };
-  }
+  meta = {
+    description = "Compiler for a strongly-typed language that compiles to JavaScript";
+    homepage = "https://github.com/purescript/purescript";
+    mainProgram = pname;
+  };
+}
