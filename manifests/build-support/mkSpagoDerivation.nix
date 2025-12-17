52223e0f4a518330408e8915ef8afe88faa4e840
Thomas Honeyman <hello@thomashoneyman.com>
2025-12-16 19:02:41 -0500

    Refactor Nix setup to be simpler (#132)

--- manifests/build-support/mkSpagoDerivation.nix
+++
@@ -1,9 +0,0 @@
-import ./mkDerivation.nix {
-  name = "spago";
-  js = "bin/bundle.js";
-  meta = lib: {
-    description = "PureScript package manager and build tool";
-    homepage = "https://github.com/purescript/spago";
-    license = lib.licenses.bsd3;
-  };
-}
